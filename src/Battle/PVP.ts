52c58a4eb504ae486350ac7925cf65df0eaf7244
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-18 19:22:46 -0300

    refactor: utilização do parâmetro player da classe pai ao inves de criar um novo na classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,19 +2,17 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  private _player1: Fighter;
   private _player2: Fighter;
 
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
-    this._player1 = player1;
     this._player2 = player2;
   }
 
   public fight(): number {
-    while (this._player1.lifePoints !== -1 && this._player2.lifePoints !== -1) {
-      this._player1.attack(this._player2);
-      this._player2.attack(this._player1);
+    while (this.player.lifePoints !== -1 && this._player2.lifePoints !== -1) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
     }
 
     return super.fight();
