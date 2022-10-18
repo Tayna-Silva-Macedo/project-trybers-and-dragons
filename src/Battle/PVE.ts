2c37b10a65be286902872ff534b8a3ecf07df7b2
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-18 19:26:30 -0300

    refactor: utilização do parâmetro player da classe pai ao inves de criar um novo na classe PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -2,17 +2,15 @@ import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
-  private _player: Fighter;
   private _monsters: SimpleFighter[];
 
   constructor(player: Fighter, monsters: SimpleFighter[]) {
     super(player);
-    this._player = player;
     this._monsters = monsters;
   }
 
   private isSomeoneDead(): boolean {
-    const isPlayerAlive = this._player.lifePoints !== -1;
+    const isPlayerAlive = this.player.lifePoints !== -1;
     const areMonstersAlive = this._monsters.some(
       (monster) => monster.lifePoints !== -1,
     );
@@ -25,8 +23,8 @@ export default class PVE extends Battle {
   public fight(): number {
     while (!this.isSomeoneDead()) {
       this._monsters.forEach((monster) => {
-        this._player.attack(monster);
-        monster.attack(this._player);
+        this.player.attack(monster);
+        monster.attack(this.player);
       });
     }
 
